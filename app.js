0666b0114fb10bbe5e69b6a27b2ad95e9e5b7a86
Mike-Monta <Grund1sc@gmail.com>
2021-06-16 09:06:35 +0200

    V0.1

---
+++ app.js
@@ -0,0 +1,8 @@
+// LOGIC
+
+const getAdd =(a,b) =>parseFloat(a)+parseFloat(b);
+const getMinus = (a,b)=> parseFloat(a)-parseFloat(b);
+const getMulti = (a,b)=> parseFloat(a)*parseFloat(b);
+const getDivide = (a,b)=> parseFloat(a)/parseFloat(b);
+
+
