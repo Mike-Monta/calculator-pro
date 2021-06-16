74da1ebd2bd1c105f3c6744cecee7b0869101a2e
Mike-Monta <Grund1sc@gmail.com>
2021-06-17 00:20:17 +0200

    Functional at last

--- app.js
+++ app.js
@@ -1,51 +1,129 @@
 // LOGIC
 
-const getAdd =(a,b) =>parseFloat(a)+parseFloat(b);
-const getMinus = (a,b)=> parseFloat(a)-parseFloat(b);
-const getMulti = (a,b)=> parseFloat(a)*parseFloat(b);
-const getDivide = (a,b)=> parseFloat(a)/parseFloat(b);
 
-// Set on screen
-
-function clrScreen(){
-    onScreen.textContent="";
-}
-function updateScreen(){
-    onScreen.textContent += key.id;
-    //screenText = parseInt(document.getElementById('screen').textContent);
-}
-
+// Set screen
 const numKeys = document.querySelectorAll('.num');
+const opKeys = document.querySelectorAll('.op');
 const key = document.querySelectorAll('.button');
 const onScreen = document.querySelector('#screen');
-const screenText = parseFloat(document.getElementById('screen').textContent);
+const onScreenOp = document.querySelector('#screenOp');
 
+let num1 = 0;
+let num2 = 0;
+let opKey= "";
+let operator="";
+let memNumber= 0;
+let screenText = 0;
+let keyId = "";
+let opId = "";
+let result = 0;
+let operationInCurse= "off";
+//document.querySelector('#screen').textContent= screenText;
+screenText = document.querySelector('#screen').textContent;
+
+function clrScreen(){
+    onScreen.textContent= 0;
+    onScreenOp.textContent = "";
+    console.log(typeof screenText);
+    //screenText = document.getElementById('screen').textContent;
+    screenText = parseFloat(document.getElementById('screen').textContent);
+}
+clrScreen();
+
+function updateScreen(set){
+    if(onScreen.textContent === 0 || onScreen.textContent === "0" ){
+        onScreen.textContent = set;
+    }else if (screenText !== 0 || screenText !== "0"){
+        onScreen.textContent += set;
+    }
+    screenText = parseFloat(document.getElementById('screen').textContent);
+}
+function resolveOp(oper){
+    console.log(num1, num2)
+    if (oper == "+"){
+         onScreen.textContent= num1+num2;
+       }
+    if (oper == "-"){
+         onScreen.textContent= num1-num2;
+       }
+    if (oper == "/"){
+         onScreen.textContent= parseFloat(num1/num2).toFixed(10);
+       }
+    if (oper == "x"){
+        onScreen.textContent= Math.round(num1*num2);
+       }
+       operator = "";
+       onScreenOp.textContent = "";
+       operationInCurse = "off";
+
+}
+//  Nums
 numKeys.forEach(key => {
     key.addEventListener('click', Event =>{
-        if (key.id === "."){
-            if (!onScreen.textContent.includes('.')){
-                onScreen.textContent += key.id;
-            }
-         } else if(key.id !=='0' && onScreen.textContent == '0') {
-                    clrScreen();
-                    onScreen.textContent += key.id;
-         }else if (key.id ==='0' && onScreen.textContent == '0'){ 
+        console.log(operationInCurse);
+        if(operationInCurse === "on"){
+            onScreen.textContent = "0";
+            operationInCurse = "off";
+        }
+        if(key.id ==="."){
+            keyId = key.id;
+        }else {
+            keyId = parseFloat(key.id);
+        }
+        console.log(typeof keyId + keyId);
+        console.log(typeof screenText + screenText);
+        if(screenText.toString().length < 12){
             
             
-         }else{
-            onScreen.textContent += key.id;
-         }
-            //onScreen.textContent.includes('.') 
-       
-        
+            if (onScreen.textContent === "0" && keyId=== "."){
+                onScreen.textContent = "0.";
+            }else {
+                if (key.id === "."){
+                     if (!onScreen.textContent.includes('.')){
+                        updateScreen(keyId);
+                    }
+                 } else if(key.id !=='0' && onScreen.textContent == 0) {
+                           //clrScreen();
+                           updateScreen(keyId);
+                 }else if (key.id ==='0' && onScreen.textContent === "0"){ 
+                                
+                 }else{
+                    updateScreen(keyId);
+                 } 
             
 
-
-
-            console.log(parseFloat(document.getElementById('screen').textContent)); 
-            console.log(screenText);
-         
+                }             
+             console.log(screenText);
+             //console.log(parseFloat(document.getElementById('screen').textContent)); 
+             //console.log(screenText.toString.length); 
+        }     
     })
-})  
+}) 
+//  Operators
 
+opKeys.forEach(key =>{
+    key.addEventListener('click', Event =>{
+        opId = key.id;
+        console.log(opId);
+        if (opId == "AC"){
+            clrScreen();
+        }
+        if (opId == "+" || opId== "-" || opId == "/" || opId == "x"){
+            num1 = parseFloat(onScreen.textContent);
+            onScreenOp.textContent = opId;
+            operator = opId;
+            operationInCurse = "on";
+        }
+        if (opId === "="){
+            num2 = parseFloat(screenText);
+            resolveOp(operator);
+            operationInCurse = "off";
+        }
+
+       // if(opId === "=" && num1 == 0){
+            
+
+        
+    })
+})
 
